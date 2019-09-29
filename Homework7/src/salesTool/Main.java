e10e49c9ab1d0beef2615f0646fa27c6f0ae7aeb
LoganBrinkleyGregory <LoganBrinkleyGregory@Logan-LaptopPC.home>
2019-09-29 16:49:56 -0400

    Initial check in of sales tool.

---
+++ Homework7/src/salesTool/Main.java
@@ -0,0 +1,24 @@
+package salesTool;
+
+public class Main {
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+		
+	}
+	
+	private static void displayGreeting() {
+//		System.out.println("Hello Happy Sales People!");
+//		System.out.println("This app shows sales data.");
+//		System.out.println("--------------------------");
+		
+		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("THIS APP SHOWS SALES DATA.");
+		
+//		System.out.println("Test 1");
+//		System.out.println("Test 2");
+	}
+	
+}
